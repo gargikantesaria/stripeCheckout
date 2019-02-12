b13509e4ce29a05ecb520bf3a5dbc982287708a6
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2019-02-12 18:38:34 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavController } from 'ionic-angular';
+declare var StripeCheckout;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  handler:any;
+  constructor(public navCtrl: NavController) { }
 
+  ionViewDidLoad(){
+    setTimeout(() => {
+      this.handler =  StripeCheckout.configure({
+        key: 'YOUR_STRIPE_KEY',
+        image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+        locale: 'auto',
+        amount: 100,
+        token: token => {
+          console.log("Token is", token)
+        }
+      })
+      console.log("See the stripe data", this.handler);
+    }, 1000)
   }
 
+  handlerOpen(){
+    this.handler.open({
+      name: 'Test App name',
+      amount: 100
+    });
+  }
+
+  @HostListener('window:popstate')
+  onPopstate() {
+    this.handler.close();
+  }
+
+  payButtonClickHandler(){
+    this.handlerOpen();
+  }
 }
